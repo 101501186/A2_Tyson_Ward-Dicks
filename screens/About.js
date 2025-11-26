dd1ece06823bb5a1f0c43b4f2ca576c1b32008f3
101501186 <101501186@georgebrown.ca>
2025-11-26 13:10:36 -0600

    Created the Screens/About page

---
+++ screens/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function AboutScreen({ route }) {
+    return(
+        <Text style={styles.title}>About This App</Text>
+    );
+
+}
