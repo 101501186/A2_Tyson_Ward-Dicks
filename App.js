0273e89560889a29019c194ef326730e4f88d8bd
101501186 <101501186@georgebrown.ca>
2025-11-26 14:16:47 -0600

    Added screens and navigation to the app.js file

--- App.js
+++ App.js
@@ -9,4 +9,15 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
 
+    return (
+   <NavigationContainer>
+     <Stack.Navigator initialRouteName="Home">
+       <Stack.Screen name="Home" component={HomeScreen} />
+       <Stack.Screen
+         name="About"
+         component={AboutScreen}
+       />
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
 }
