9d91b6559930d30f404a05739a2e69bfa27c6ddc
101501186 <101501186@georgebrown.ca>
2025-11-26 13:16:38 -0600

    Added my name to the About page

--- screens/About.js
+++ screens/About.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function AboutScreen({ route }) {
+    const { name, studentId } = route.params || {};
     return(
-        <Text style={styles.title}>About This App</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>About This App</Text>
+
+            <Text style={styles.text}>Name: {name || 'Tyson Ward-Dicks'}</Text>
+        </View>
     );
 
 }
