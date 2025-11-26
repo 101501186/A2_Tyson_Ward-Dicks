a2f74c842ffae9708a15b1222ef1a3da1c6c3dea
101501186 <101501186@georgebrown.ca>
2025-11-26 13:45:37 -0600

    initializing state variables for the home page

--- screens/Home.js
+++ screens/Home.js
@@ -10,5 +10,10 @@ import {
 const API_KEY = 'fca_live_JxwBC4Qa17X1jy9yzfpWoEWYlSNmsfK7MgBOb0wC';
 
 export default function HomeScreen({ navigation }) {
-
+    const [baseCurrency, setBaseCurrency] = useState('CAD'); // default is CAD
+    const [destCurrency, setDestCurrency] = useState('');
+    const [amount, setAmount] = useState('1'); // default is 1
+    const [rate, setRate] = useState(null);
+    const [result, setResult] = useState(null);
+    const [error, setError] = useState('');
 }    
