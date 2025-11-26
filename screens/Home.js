0bdb6910d438c6d99a6202562fede96409b5efc0
101501186 <101501186@georgebrown.ca>
2025-11-26 13:47:34 -0600

    Added input validation to the home page

--- screens/Home.js
+++ screens/Home.js
@@ -16,4 +16,33 @@ export default function HomeScreen({ navigation }) {
     const [rate, setRate] = useState(null);
     const [result, setResult] = useState(null);
     const [error, setError] = useState('');
+
+
+    const validateInputs = () => {
+        const base = baseCurrency.trim().toUpperCase();
+        const dest = destCurrency.trim().toUpperCase();
+        const amtNumber = parseFloat(amount);
+
+        // currency code validation
+        const codeRegex = /^[A-Z]{3}$/;
+
+        if (!codeRegex.test(base) || !codeRegex.test(dest)) {
+            setError(
+            'Currency codes must be 3-letter uppercase codes (e.g., CAD, USD, EUR).'
+            );
+            setRate(null);
+            setResult(null);
+            return null;
+        }
+
+        if (isNaN(amtNumber) || amtNumber <= 0) {
+            setError('Amount must be a positive number.');
+            setRate(null);
+            setResult(null);
+            return null;
+        }
+
+        setError('');
+        return { base, dest, amtNumber };
+ };
 }    
